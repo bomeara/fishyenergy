419c059fd29395169cc4a607e32a65689a52f7b3
Matthew Troia <matthew.troia@utsa.edu>
2026-06-24 11:58:11 -0500

    update sensitivity function

--- R/fishyenergy.R
+++ R/fishyenergy.R
@@ -1189,10 +1189,12 @@ bem_corroborate <- function(start_L = 10, end_L.empirical, temperature, parms.in
 #' @param temperature a dataframe populated with a time series of mean daily water temperature (degrees C x 10) of a habitat patch.
 #' @param parms.intrinsic A parms.intrinsic object; note that temperature dependent parameters are bypassed if C_eq = 4 and/or R_eq = 4.
 #' @param parms.temporal a dataframe populated with a time series of intrinsic (e.g., GSI) and extrinsic (e.g., prey energy density) biological parameters.
+#' @param C_eq Specify consumption equation 1, 2, or 3 from Hanson et al. 1997 or equation 4.
+#' @param R_eq Specify respiration equation 1 or 2 from Hanson et al. 1997.
 #' @param parm_dev Specify specify the percent deviation for each intrinsic parameter.
 #' @return a list of length 2; table of parameter sensitivities; plotted parameter sensitivies.
 #' @export
-bem_sensitive <- function(start_M2 = 100, temperature, parms.intrinsic, parms.temporal, parm.dev)
+bem_sensitive <- function(start_M2 = 100, temperature, parms.intrinsic, parms.temporal, C_eq, R_eq, parm.dev)
 {
   # set parm deviation
   parm_dev <- parm.dev / 100
@@ -1217,12 +1219,12 @@ bem_sensitive <- function(start_M2 = 100, temperature, parms.intrinsic, parms.te
   # loop through i parms and j values of each parm
   for(i in 1:length(list.parms_sens)){
     for(j in 1:nrow(list.parms_sens[[i]])){
-      df.output[j,i] <- bem_grow(start_M2 = 454,
-                                 temperature = temps_fake,
+      df.output[j,i] <- bem_grow(start_M2,
+                                 temperature,
                                  parms.intrinsic = list.parms_sens[[i]][j,],
-                                 parms.temporal = parms_temporal_DEFAULT,
-                                 C_eq = 2,
-                                 R_eq = 1)[365,"M2_cum"]
+                                 parms.temporal,
+                                 C_eq,
+                                 R_eq)[nrow(temperature),"M2_cum"]
     }
   }
 
